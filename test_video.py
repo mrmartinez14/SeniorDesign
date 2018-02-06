4139c2ea62ea23d18e5272d9e2af3150a560a8ad
Miguel Martinez <mrmartinez14@my.trine.edu>
2018-02-05 20:33:20 -0500

    added exception handling to turn off motors when exceptions are thrown

--- test_video.py
+++ test_video.py
@@ -4,6 +4,7 @@ import cv2
 import numpy as np
 import time
 from dual_mc33926_rpi import motors, MAX_SPEED
+import wiringpi as wpi
 
 # initialize the camera and grab a reference to the raw camera capture
 camera = PiCamera()
@@ -29,37 +30,54 @@ upper_green = np.array([90,255,255])
 # allow the camera to warmup
 time.sleep(0.1)
 
+# Setup GPIO
+try:
+    wpi.wiringPiSetupGpio()
+except:
+    print('GPIO already initialized')
+
 # capture frames from the camera
-for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
-    if first:
-        motors.enable()
-        motors.setSpeeds(MAX_SPEED, MAX_SPEED)
-        first = False
-    # grab the raw NumPy array representing the image, then initialize the timestamp
-    # and occupied/unoccupied text
-    image = frame.array
+try:
+    for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
+        if first:
+            motors.enable()
+            motors.setSpeeds(MAX_SPEED, MAX_SPEED)
+            first = False
+        # grab the raw NumPy array representing the image, then initialize the timestamp
+        # and occupied/unoccupied text
+        image = frame.array
 
-    # Convert BGR to HSV
-    hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+        # Convert BGR to HSV
+        hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
 
-    # Get the mask for the current state
-    if state == 'h':
-        mask = cv2.inRange(hsv, lower_green, upper_green)
-    if state == 'c':
-        mask = cv2.inRange(hsv, lower_yellow, upper_yellow)
-    if state == 'b':
-        mask = cv2.inRange(hsv, lower_blue, upper_blue)
-    if state == 'f':
-        mask = cv2.inRange(hsv, lower_red, upper_red)
+        # Get the mask for the current state
+        if state == 'h':
+            mask = cv2.inRange(hsv, lower_green, upper_green)
+        if state == 'c':
+            mask = cv2.inRange(hsv, lower_yellow, upper_yellow)
+        if state == 'b':
+            mask = cv2.inRange(hsv, lower_blue, upper_blue)
+        if state == 'f':
+            mask = cv2.inRange(hsv, lower_red, upper_red)
 
-    cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[-2]
+        cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[-2]
 
-    if len(cnts) > 0:
-        c = max(cnts, key=cv2.contourArea)
-        for i in c.tolist():
-            xCoord = xCoord + i[0][0]
-            yCoord = yCoord + i[0][1]
-        print(xCoord/len(c), yCoord/len(c))
-    xCoord = 0
-    yCoord = 0
-    rawCapture.truncate(0)
+        if len(cnts) > 0:
+            c = max(cnts, key=cv2.contourArea)
+            for i in c.tolist():
+                xCoord = xCoord + i[0][0]
+                yCoord = yCoord + i[0][1]
+            print(xCoord/len(c), yCoord/len(c))
+
+        xCoord = 0
+        yCoord = 0
+        rawCapture.truncate(0)
+
+except KeyboardInterrupt:
+    print('Ending program')
+
+finally:
+    print('finally')
+    motors.setSpeeds(0,0)
+    motors.disable()
+
