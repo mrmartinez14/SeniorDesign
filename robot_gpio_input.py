b705dd0ecc3639fbba7ace186a86308c6e1391c4
Miguel Martinez <mrmartinez14@my.trine.edu>
2018-02-28 17:23:19 -0500

    progress

--- robot_gpio_input.py
+++ robot_gpio_input.py
@@ -3,9 +3,9 @@ from picamera import PiCamera
 import cv2
 import numpy as np
 import time
-from dual_mc33926_rpi import motors, MAX_SPEED
 import wiringpi as wpi
 import color
+from state_machine import stateMachine
 
 # initialize the camera and grab a reference to the raw camera capture
 width = color.WIDTH
@@ -21,7 +21,7 @@ start_button = 21
 prox_sensor = 26
 count = 0
 reset = True
-
+sm = stateMachine()
 # allow the camera to warmup
 time.sleep(0.1)
 
@@ -31,17 +31,16 @@ wpi.pinMode(start_button, wpi.INPUT)
 wpi.pinMode(prox_sensor, wpi.INPUT)
 
 # Wait for start button
-#while wpi.digitalRead(start_button) == 0:
-#    print('wait to start')
+while wpi.digitalRead(start_button) == 1:
+    print('wait to start')
 
+#sm.get_rolling(.1)
 # capture frames from the camera
 try:
     for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
-        if first:
-            motors.enable()
-            #motors.setSpeeds(MAX_SPEED, MAX_SPEED)
-            motors.setSpeeds(0,0)
-            first = False
+        if wpi.digitalRead(start_button) == 1:
+            raise KeyboardInterrupt
+
         # grab the raw NumPy array representing the image, then initialize the timestamp
         # and occupied/unoccupied text
         image = frame.array
@@ -49,23 +48,8 @@ try:
         mask = color.get_mask(state, image)
 
         xCoord = color.get_position(mask)
-
-#        print(xCoord)
-        #left
-        if(xCoord > (3 * width/5)):
-            motors.setSpeeds(-310,300)
-#            print('l')
-        #in front
-        elif(xCoord < (3* width / 5)) and (xCoord > (2 * width/5)):
-            motors.motor2.setSpeed(300)
-#            time.sleep(0.01)
-            motors.motor1.setSpeed(-300)
-#            print('s')
-#        elif(xCoord <400) and (xCoord > 250):
-        else:
-            motors.setSpeeds(-290,300)
-#            print('r')
-        #right
+        sm.set_motors(sm.get_next_state(xCoord))
+        print(xCoord)
 
  #       if wpi.digitalRead(prox_sensor) == 0 and reset == True:
  #           if state == 'h':
@@ -99,6 +83,4 @@ except KeyboardInterrupt:
 
 finally:
     print('finally')
-    motors.setSpeeds(0,0)
-    motors.disable()
-
+    sm.kill_motors()
--- state_machine.py
+++ state_machine.py
@@ -1,4 +1,6 @@
 from enum import Enum
+from dual_mc33926_rpi import motors, MAX_SPEED
+import time
 
 class states(Enum):
     straight_turn_s = 0
@@ -8,10 +10,78 @@ class states(Enum):
     right_turn_s = 4
     left_turn_l = 5
     left_turn_s = 6
+    turn = 7
 
-class state_machine:
+class stateMachine:
 
 
     def __init__(self):
-        current_state = states.straight_turn_s
-        next_state = states.straight_turn_s
+        self.motors = motors
+        self.current_state = states.straight_turn_s
+        self.motors.setSpeeds(0,0)
+        self.motors.enable()
+        self.count = 0
+
+    def update_state(self, coord, motors_on):
+#        if self.count < 10:
+#            self.count = self.count + 1
+        if self.current_state == states.straight_turn_s:
+            self.current_state = get_next_state(coord)
+        elif self.current_state == states.straight_turn_l:
+            self.current_state = states.straight_turn_s
+        elif self.current_state == states.straight_turn_r:
+            self.current_state = states.straight_turn_s
+        elif self.current_state == states.left_turn_l:
+            self.current_state = states.left_turn_s
+        elif self.current_state == states.right_turn_r:
+            self.current_state = states.right_turn_s
+        elif self.current_state == states.left_turn_s:
+            self.current_state = states.straight_turn_r
+#            self.count = 0
+        elif self.current_state == states.right_turn_s:
+            self.current_state = states.straight_turn_l
+#            self.count = 0
+        elif self.current_State == states.straight_turn_r:
+            self.current_state = states.straight_turn_s
+        elif self.current_State == states.straight_turn_l:
+            self.current_state = states.straight_turn_s
+        if motors_on:
+            self.set_motors(self.current_state)
+#        else:
+        print self.current_state
+
+    def kill_motors(self):
+        self.motors.disable()
+        self.motors.setSpeeds(0,0)
+
+    def set_motors(self, state):
+        if state == states.straight_turn_s:
+            self.motors.setSpeeds(-250,-250)
+        elif state == states.straight_turn_l:
+            self.motors.setSpeeds(150,-480)
+        elif state == states.straight_turn_r:
+            self.motors.setSpeeds(-480,150)
+        elif state == states.right_turn_r:
+            self.motors.setSpeeds(-480,150)
+        elif state == states.right_turn_s:
+            self.motors.setSpeeds(-250,-250)
+        elif state == state.left_turn_l:
+            self.motors.setSpeeds(150,-480)
+        elif state == state.left_turn_s:
+            self.motors.setSpeeds(-250,-250)
+
+    def reset_motors(self):
+        self.motors.enable()
+
+    def get_rolling(self, duration):
+        self.motors.setSpeeds(-250,-250)
+        time.sleep(duration)
+        return
+
+    def get_next_state(self, coord):
+        if coord <= 614:
+            return states.right_turn_r
+        elif coord > 614 and coord <= 820:
+            return states.straight_turn_s
+        elif coord > 820:
+            return states.left_turn_l
